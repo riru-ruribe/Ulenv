14edf67ff1fd6b114cc9dc03b45a079808832321
riru-ruribe <riru.ruribe@gmail.com>
2026-01-14 11:07:09 +0900

    購読機能

---
+++ UlObservableGenerator/UlObservableGenerator/DiagnosticDescriptors.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace UlObservableGenerator;
+
+public static class DiagnosticDescriptors
+{
+    const string Category = "UlObservable";
+
+    public static readonly DiagnosticDescriptor E0001 = new(
+        id: Category + nameof(E0001),
+        title: "invalid accessibility",
+        messageFormat: "'public' or 'protected' or 'internal' or 'private' is allowed.",
+        category: Category,
+        defaultSeverity: DiagnosticSeverity.Error,
+        isEnabledByDefault: true
+    );
+
+    public static readonly DiagnosticDescriptor E0002 = new(
+        id: Category + nameof(E0002),
+        title: "invalid syntax",
+        messageFormat: "'partial' class required.",
+        category: Category,
+        defaultSeverity: DiagnosticSeverity.Error,
+        isEnabledByDefault: true
+    );
+
+    public static readonly DiagnosticDescriptor E0003 = new(
+        id: Category + nameof(E0003),
+        title: "invalid syntax",
+        messageFormat: "method attribute is empty.",
+        category: Category,
+        defaultSeverity: DiagnosticSeverity.Error,
+        isEnabledByDefault: true
+    );
+}
---
+++ UlenvKit/UlenvKit/IUlObservable.cs
@@ -0,0 +1,9 @@
+﻿namespace Ulenv;
+
+/// <summary>
+/// ユニーク監視可能モデルです
+/// </summary>
+public interface IUlObservable
+{
+    void Subscribe<T>(IResolvable observer) where T : unmanaged, IUlCallback<Unique>;
+}
